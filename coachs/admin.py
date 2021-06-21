a609024f4e851bf3badc0f7abbc9dd36904a0528
Diqxy1 <kusanagitakeru@outlook.com>
2021-06-21 18:00:19 -0300

    feat: added coach app

---
+++ coachs/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Coach, Evaluation
+
+
+@admin.register(Coach)
+class CoachAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'created_at', 'updated_at', 'is_active')
+
+
+@admin.register(Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = ('coach', 'name', 'email', 'evaluation', 'created_at', 'updated_at', 'is_active')
---
+++ coachs/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Coach(Base):
+    title = models.CharField(max_length=120)
+    description = models.TextField(max_length=1000)
+    url = models.URLField(unique=True)
+
+    class Meta:
+        verbose_name = 'coach'
+        verbose_name_plural = 'coachs'
+
+    def __str__(self):
+        return self.title
+
+
+class Evaluation(Base):
+    coach = models.ForeignKey(Coach, related_name='avaliacoes', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    comment = models.TextField(blank=True, default='')
+    evaluation = models.DecimalField(max_digits=2, decimal_places=1)
+
+    class Meta:
+        verbose_name = 'Avaliação',
+        verbose_name_plural = 'Avaliações'
+        unique_together = ['email', 'coach']
+
+    def __str__(self):
+        return f'{self.name} avaliou o curso {self.coach} com nota {self.evaluation}'
