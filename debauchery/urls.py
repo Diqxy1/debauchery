a07d45091fd7f91d61b556e6638baaa1eb262669
Diqxy1 <kusanagitakeru@outlook.com>
2021-06-22 19:10:31 -0300

    feat: added api-v2 in urls

--- debauchery/urls.py
+++ debauchery/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from coachs.urls import router
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
-    path('api/v1/', include('coachs.urls')),
+    path('api/v2/', include(router.urls)),
 ]
