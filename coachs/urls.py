bb856f36257c0e21d70a3b41d06cc4efd094b8b0
Diqxy1 <kusanagitakeru@outlook.com>
2021-06-22 19:10:01 -0300

    refactor: using only router

--- coachs/urls.py
+++ coachs/urls.py
@@ -1,15 +1,9 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
 
-from .views import CoachsAPIView, CoachAPIView, AssessmentsAPIView, AssessmentAPIView
+from .views import CoachViewSet, ReviewViewSet
 
-urlpatterns = [
-    # coachs routes
-    path('coachs/', CoachsAPIView.as_view(), name='coachs'),
-    path('coachs/<int:pk>/', CoachAPIView.as_view(), name='coach'),
-    path('coachs/<int:coach_pk>/avaliacoes/', AssessmentsAPIView.as_view(), name='coach_avaliacoes'),
-    path('coachs/<int:coach_pk>/avaliacoes/<int:assessment_pk>/', AssessmentsAPIView.as_view(),
-         name='coach_avaliacoes'),
-    # evaluation rotes
-    path('avaliacoes/', AssessmentAPIView.as_view(), name='avaliacoes'),
-    path('avaliacoes/<int:assessment_pk>/', AssessmentAPIView.as_view(), name='avaliacoes')
-]
+
+router = SimpleRouter()
+router.register('coachs', CoachViewSet)
+router.register('avaliacoes', ReviewViewSet)
