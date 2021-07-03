62bb9cb2cbaa58257f5e129c4bf86258c119b7e3
Diqxy1 <kusanagitakeru@outlook.com>
2021-07-02 21:51:14 -0300

    refactor: added authentication system

--- coachs/views.py
+++ coachs/views.py
@@ -1,15 +1,16 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import mixins
 
+from accounts.permissions import IsOwner
 from .models import Coach, Review
 from .serializers import CoachSerializer, ReviewSerializer
-from .permissions import OnlyAdmin
 
 
 class CoachViewSet(viewsets.ModelViewSet):
-    permission_classes = OnlyAdmin
+    permission_classes = (IsAuthenticated, IsOwner)
     queryset = Coach.objects.all()
     serializer_class = CoachSerializer
 
@@ -18,10 +19,12 @@ class CoachViewSet(viewsets.ModelViewSet):
         self.pagination_class.page_size = 1
         reviews = Review.objects.filter(coach_id=pk)
         page = self.paginate_queryset(reviews)
+
         if page is not None:
             serializer = ReviewSerializer(page, many=True)
             return self.get_paginated_response(serializer.data)
         serializer = ReviewSerializer(reviews, many=True)
+
         return Response(serializer.data)
 
 
